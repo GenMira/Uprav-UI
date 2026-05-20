d00f99f0e3fc5f9eb879dc6c7f182082059f5df8
genMira <{weilianzhuantongyin@gmail.com}>
2026-05-20 15:49:38 +0900

    ログインサイアンップ間のリンクを追加

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 interface SignupRequest{
   name: string;
@@ -44,11 +45,11 @@ export default function SignUp() {
 
   return(
     <div className="flex flex-col h-screen w-full bg-gray-100 justify-center items-center">
-      <div className="flex flex-col w-[50%] h-[65%] justify-center bg-white items-center gap-5 p-6 rounded-xl">
-        <h1 className="text-4xl font-bold text-gray-800 mb-6">
+      <div className="flex flex-col w-[50%] justify-center bg-white items-center gap-5 p-6 rounded-xl">
+        <h1 className="text-4xl font-bold text-gray-800">
           新規登録
         </h1>
-        <div className="flex flex-col w-full justify-center items-center mt-15 gap-5">
+        <div className="flex flex-col w-full justify-center items-center mt-10 gap-5">
           <label className="w-[60%] items-start font-semibold">
             ユーザーネーム
           </label>
@@ -60,7 +61,7 @@ export default function SignUp() {
             className="w-[60%] p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-        <div className="flex flex-col w-full justify-center items-center  mt-15 gap-5">
+        <div className="flex flex-col w-full justify-center items-center  mt-5 gap-5">
           <label className="w-[60%] items-start font-semibold ">
             password
           </label>
@@ -72,8 +73,14 @@ export default function SignUp() {
             className="w-[60%] p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
+        <Link 
+          to="/login" 
+          className="text-blue-500 font-semibold hover:underline"
+        >
+          ログインはこちら
+        </Link>
         <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 mt-15"
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 mt-8"
           onClick={() => Signup()}
         >
           登録
