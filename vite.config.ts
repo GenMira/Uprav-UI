974d54687f44953f692fa5d079f64bb32890b7c8
genMira <{weilianzhuantongyin@gmail.com}>
2026-06-09 20:12:43 +0900

    公開用設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Uprav-UI",
+  base: "/Uprav-UI/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
